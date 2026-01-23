945d6d91fb378ac58ecbb39ccd7eddaee8c1fd25
Demian <demianflury@proton.me>
2026-01-23 13:15:31 +0100

    add project

---
+++ src/test/java/io/github/demianflury/jpi/JpiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.demianflury.jpi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
